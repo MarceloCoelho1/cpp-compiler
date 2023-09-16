ab8076a1e4cb72f1369cea06ffb7ca0397a8121b
PedroPepeu <pedrosteamgtav@gmail.com>
2023-09-15 21:32:03 -0300

    started doing text, need some fixes more

--- main.cpp
+++ main.cpp
@@ -69,3 +69,21 @@ int main() {
 
     return 0;
 }
+
+std::string text(std::vector<char> code) {
+    std::string text = "";
+    int check = 0;
+
+    if(code.empty()) return ""; // if code is empty return an empty string
+
+    for(auto &x : code) { // run through code checking if has an text start with
+                          // checking the double aspas(or simple one) and end with it
+        if(code == '\"' || code == '\'') check < 1 ? check++ : check--;
+        // need to add the simple aspas and the double ones
+
+        if(check == 1) {
+            text+=x; // if check is ok, add the text to the string as it is written
+        }
+    }
+    return text; // return the text written
+}
