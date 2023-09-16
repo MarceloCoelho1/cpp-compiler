0cd8bcee96aa4cc28f8745b69aa1305f11d31111
marcelo <mhbcoelho99@gmail.com>
2023-09-15 21:15:07 -0300

    fixing bugs in handle text

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main() {
     std::regex LETTERS("[a-zA-Z]");
-    std::string text = "120.10";
+    std::string text = "Marcelo henrique brito coelho";
     std::regex NUMBERS("[0-9]");
 
     std::vector<char> code(text.begin(), text.end());
@@ -13,12 +13,14 @@ int main() {
 
     while (!code.empty()) {
         if(std::regex_match(std::string(1, code.front()), LETTERS)) {
-            std::string t("");
-            while(!code.empty() && std::regex_match(std::string(1, code.front()), LETTERS)) {
-                t += code.front();
+            std::string text("");
+            while(!code.empty() && std::regex_match(std::string(1, code.front()), LETTERS) || !code.empty() && code.front() == ' ') {
+                text += code.front();
                 code.erase(code.begin());
             }
-            std::cout << t << std::endl;
+
+
+            std::cout << text << std::endl;
         }
         if(code.front() == '\n' || code.front() == ' ') {
             code.erase(code.begin());
