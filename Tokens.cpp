d3716db49ab6c4be70fd2dff43b2ef0c74d5188c
marcelo <mhbcoelho99@gmail.com>
2023-09-16 12:15:20 -0300

    adding get methods in token class

--- Tokens.cpp
+++ Tokens.cpp
@@ -1,19 +1,14 @@
-enum TokenType {NUM_DEC, NUM_INT, ID, TEXT};
+#include "Tokens.h"
 
-template <typename T>
-
-class Tokens {
-    
-    TokenType type;
-    T value;
-
-
-    public:
-    
-    Tokens(TokenType type, T value) {
-        this->type = type;
-        this->value = value;
-    }
-
+Tokens::Tokens(TokenType type, const std::string& value) {
+    this->type = type;
+    this->value = value;
 }
 
+TokenType Tokens::getType() const {
+    return this->type;
+}
+
+const std::string& Tokens::getValue() const {
+    return this->value;
+}
